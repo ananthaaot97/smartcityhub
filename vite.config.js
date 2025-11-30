ac51ce622df649eeb76960ceb8f47da3550e2976
ananthaaot97 <ananthasaiguptha97@gmail.com>
2025-11-30 06:30:11 +0530

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
  export default defineConfig({
-  base: '/smartcity/',  // Replace with your actual repo name
+  base: '/smartcityhub/',  // Replace with your actual repo name
   plugins: [react()],
 })
